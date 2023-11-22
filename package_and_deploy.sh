b6911942472b44863a7d94453f9ba0d97c1ed45d
CarlosPGL <carlos@playgroundlabs.io>
2023-11-22 16:12:17 -0600

    Update comments

--- package_and_deploy.sh
+++ package_and_deploy.sh
@@ -4,7 +4,7 @@
 FUNCTION_NAME="txns_processor_and_emailer"
 ZIP_FILE="txns_processor_and_emailer.zip"
 
-# Package your Python code and dependencies into a ZIP file
+# Package the Python script into a ZIP file
 zip -r $ZIP_FILE transactions.py
 
 # Upload the ZIP file to AWS Lambda
